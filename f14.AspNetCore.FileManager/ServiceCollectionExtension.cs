e3f992c94797f81ee5e66680a29d3c30301a47fb
f14shm4n <uraborg@gmail.com>
2017-10-27 19:01:48 +0300

    [added] IServiceCollection extension.

---
+++ f14.AspNetCore.FileManager/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace f14.AspNetCore.FileManager
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// Adds all f14 file manager services with base configuration.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddF14FileManager(this IServiceCollection services)
+        {            
+            services.AddSingleton<RequestHandlerManager>();
+            return services;
+        }
+        /// <summary>
+        /// Adds all f14 file manager services with user configuration.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configure">User configuration.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddF14FileManager(this IServiceCollection services, Action<RequestHandlerManager> configure)
+        {
+            services.AddSingleton((provider) =>
+            {
+                RequestHandlerManager manager = new RequestHandlerManager();
+                configure(manager);
+                return manager;
+            });
+            return services;
+        }
+    }
+}
