fde67d8d31c58c5dfb45b8cc5d395447cee89e88
f14shm4n <uraborg@gmail.com>
2017-12-15 19:11:21 +0300

    [added, modified] Renamed service collection methods. Adds new service collection method.

--- f14.AspNetCore.FileManager/ServiceCollectionExtension.cs
+++ f14.AspNetCore.FileManager/ServiceCollectionExtension.cs
@@ -10,11 +10,11 @@ namespace f14.AspNetCore.FileManager
     public static class ServiceCollectionExtension
     {
         /// <summary>
-        /// Adds defaults file manager handlers.
+        /// Adds defaults file manager handlers as the <see cref="IOperationHandler{T}"/>.
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
-        public static IServiceCollection AddF14FileManager(this IServiceCollection services)
+        public static IServiceCollection AddFileManagerHandlers(this IServiceCollection services)
         {
             services.AddTransient<IOperationHandler<CopyParam>, CopyHandler>();
             services.AddTransient<IOperationHandler<CreateFolderParam>, CreateFolderHandler>();
@@ -22,6 +22,21 @@ namespace f14.AspNetCore.FileManager
             services.AddTransient<IOperationHandler<FolderStructParam>, FolderStructHandler>();
             services.AddTransient<IOperationHandler<MoveParam>, MoveHandler>();
             services.AddTransient<IOperationHandler<RenameParam>, RenameHandler>();
+            return services;
+        }
+        /// <summary>
+        /// Adds defaults file manager handlers as the <see cref="IJOperationHandler{T}"/> to use build-in json parser functions.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddFileManagerJsonFileManager(this IServiceCollection services)
+        {
+            services.AddTransient<IJOperationHandler<CopyParam>, CopyHandler>();
+            services.AddTransient<IJOperationHandler<CreateFolderParam>, CreateFolderHandler>();
+            services.AddTransient<IJOperationHandler<DeleteParam>, DeleteHandler>();
+            services.AddTransient<IJOperationHandler<FolderStructParam>, FolderStructHandler>();
+            services.AddTransient<IJOperationHandler<MoveParam>, MoveHandler>();
+            services.AddTransient<IJOperationHandler<RenameParam>, RenameHandler>();
             return services;
         }
     }
